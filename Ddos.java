1fb32b7130014ff0d8bc84c39bc7b1f8c6b30083
Mahamud570 <74396542+Mahamud570@users.noreply.github.com>
2023-10-12 20:45:59 +0600

    Ddos.java

--- Ddos.java
+++ Ddos.java
@@ -66,7 +66,7 @@ public class Dos implements Runnable {
             dos.sslCheckConnection(url);
         }
 
-        System.out.println("Setting DDoS By: Shadow Tak");
+        System.out.println("Setting DDoS By: Imposter");
 
         System.out.print("Thread: ");
         String amount = in.nextLine();
